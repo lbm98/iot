c9fafee849fb425212b918c0189899886371abbe
lbm <lars.moons@student.uantwerpen.be>
2023-05-16 11:31:03 +0200

    more

--- coral/ble/main.py
+++ coral/ble/main.py
@@ -17,7 +17,7 @@ CHARACTERISTIC_UUID = 0x2000
 # The coral collects data from the sensors
 # and will send it to the cloud.
 # Here we define parameters related to the cloud connection.
-CLOUD_HOST = '192.168.1.28'  # Could change because of DHCP
+CLOUD_HOST = '192.168.248.58'  # Could change because of DHCP
 CLOUD_PORT = 8080
 CLOUD_PATH = '/sensor'
 COULD_URL = f'http://{CLOUD_HOST}:{CLOUD_PORT}{CLOUD_PATH}'
